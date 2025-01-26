d183866c3a3947ae1fd023e51b635aa4842fd235
fabio <>
2025-01-26 18:22:46 -0300

    removed old serveless dir

--- serveless/telegrambot/app.py
+++
@@ -1,9 +0,0 @@
-from helpers import Boto3Client,TelegramBotHandler
-from typing import Dict, Any
-boto3_client = Boto3Client()
-
-bot_token = boto3_client.get_parameter("/Telegram/TokenBot")
-telegram_bot_handler = TelegramBotHandler(bot_token)
-
-def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
-    return telegram_bot_handler.lambda_handler(event, context)
